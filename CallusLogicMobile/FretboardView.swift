87e8e69ea9a657ad6eec7ddfbfb147b438de7e6b
Ben Gire <bmgire@gmail.com>
2017-12-11 21:45:57 -0800

    First Commit

---
+++ CallusLogicMobile/FretboardView.swift
@@ -0,0 +1,20 @@
+//
+//  FretboardView.swift
+//  CallusLogicMobile
+//
+//  Created by Ben Gire on 12/11/17.
+//  Copyright © 2017 Gire. All rights reserved.
+//
+
+import UIKit
+
+class FretboardView: UIImageView {
+
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+/*    override func draw(_ rect: CGRect) {
+
+    }
+*/
+
+}
