09a5343ea77e7ac8d30149dc0ea652b280b0e028
Ben Gire <bmgire@gmail.com>
2018-03-07 10:57:21 -0800

    Added FBCollectionAndIndex class with delegate protocol to receive FBCollectionAndIndex objects. Passing objects between VC works.

--- CallusLogicMobile/ViewControllers/FBSelectionVC.swift
+++ CallusLogicMobile/ViewControllers/FBSelectionVC.swift
@@ -8,27 +8,54 @@
 
 import UIKit
 
+/*
 protocol FBSelectionDelegate: class {
     func fbCollectionChanged(collection: FBCollectionModel)
 }
+*/
 
+class FBSelectionVC: UIViewController, UITableViewDelegate, UITableViewDataSource, FBCollectionAndIndexDelegate {
 
-class FBSelectionVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
-
+    
+    func receive(collectionAndIndex: FBCollectionAndIndex) {
+        //
+        fbCollectionAndIndex = collectionAndIndex
+    }
+    
+  var delegate: FBCollectionAndIndexDelegate?
+    
+    
+    //var delegate: FBSelectionDelegate?
     
     // Users will select these to view a fretboard model.
     var arrayOfFBCollectionModels = [FBCollectionModel]()
+    //var fbCollectionToLoad: FBCollectionModel?
+    //var fbCollectionToLoadIndex = 0
+    
+    var fbCollectionAndIndex = FBCollectionAndIndex()
+    
     
     @IBOutlet var collectionTableView: UITableView!
     
     @IBOutlet var image: UIImageView!
     
     @IBAction func viewSelectedFBAction(_ sender: UIButton) {
+        
     }
     override func viewDidLoad() {
         super.viewDidLoad()
         arrayOfFBCollectionModels.append(FBCollectionModel())
+        arrayOfFBCollectionModels[0].arrayOfFretboardModels.append(FretboardModel())
+        let indexPath = IndexPath(row: 0, section: 0)
+        collectionTableView.selectRow(at: indexPath, animated: false, scrollPosition: .top)
         // Do any additional setup after loading the view.
+        
+        // Load the fbCollection passed to this VC.
+        if let newCollection = fbCollectionAndIndex.collection {
+            arrayOfFBCollectionModels[fbCollectionAndIndex.index] = newCollection
+        }
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -59,9 +86,34 @@ class FBSelectionVC: UIViewController, UITableViewDelegate, UITableViewDataSourc
         return cell
     }
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // TODO: add code to show image and edit buttons
+        
+        // update collectionIndex
+        
+    }
     // TableView Delegate Methods.
     // These methods prompt the image and options to edit the fretboardCollectionTitle.
     
-    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "segueToFBViewController" {
+            let vc = segue.destination as! FBViewController
+            // update after I setup the tableView did select row
+            if let index = collectionTableView?.indexPathForSelectedRow?.row {
+                // setup FBCollectionAndIndex
+                fbCollectionAndIndex.collection = arrayOfFBCollectionModels[index]
+                fbCollectionAndIndex.index = index
+                
+                // Set delegate and pass index
+                delegate = vc
+                delegate?.receive(collectionAndIndex: fbCollectionAndIndex)
+                
+                
+            } else {
+                print("\(#function): No row is selected, indexPath was nil")
+            }
+            
+        }
+    }
     
 }
