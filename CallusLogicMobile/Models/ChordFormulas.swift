919f25627f4d42fed53e09f7cb437b9519fac5b0
Ben Gire <bmgire@gmail.com>
2018-03-23 16:44:26 -0700

    A basically outline of the chord implementation is working. But needs heavy refinement. But first fix the crashes.
    The app is still crashing.
    I need to fix the Fretboard models to save the Scales or basic chords segmented controls.

---
+++ CallusLogicMobile/Models/ChordFormulas.swift
@@ -0,0 +1,25 @@
+//
+//  ChordFormulas.swift
+//  CallusLogicMobile
+//
+//  Created by Ben Gire on 3/23/18.
+//  Copyright © 2018 Gire. All rights reserved.
+//
+
+import Foundation
+
+
+class ChordFormulas {
+    
+    var arrayOfArraysOfChordFormulas = [[String]]()
+    var arrayOfChordNames = [String]()
+    var dictOfChordNamesAndFormulas = [ String : [String] ]()
+    
+    init() {
+        let minor_OnLowE = [ "root", "P5", "root", "m3", "P5", "root"]
+        arrayOfArraysOfChordFormulas.append(minor_OnLowE)
+        arrayOfChordNames.append("minor_OnLowE")
+        dictOfChordNamesAndFormulas[arrayOfChordNames[0]] = arrayOfArraysOfChordFormulas[0]
+    }
+    
+}
