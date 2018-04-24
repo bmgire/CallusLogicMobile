884e52a82c1aa9610a7c58f65e521ce5ab7b7390
Ben Gire <bmgire@gmail.com>
2018-04-24 12:06:54 -0700

    commiting .gitignore, assets 15_Fret_fretboard.png, and enums. before I try to merge

---
+++ CallusLogicMobile/Enums/DisplayMode.swift
@@ -0,0 +1,22 @@
+//
+//  DisplayModeEnums.swift
+//  CallusLogicMobile
+//
+//  Created by Ben Gire on 3/1/18.
+//  Copyright © 2018 Gire. All rights reserved.
+//
+
+import Foundation
+
+enum DisplayMode: String {
+    case notes = "Notes" //
+    case fretNumbers = "Fret Numbers"
+    case intervals = "Intervals"
+    case chordFingers = "Chord Fingers"
+    case numbers0to11 = "Numbers 0-11"
+    case numbers0to39 = "Numbers 0-39"
+    //   case numbers0to36 = "Numbers 0-36"
+}
+
+
+//["Notes", "Fret Numbers","Intervals", "Chord Fingers", "Numbers 0-11", "Numbers 0-36"]
---
+++ CallusLogicMobile/Enums/FretboardValues.swift
@@ -0,0 +1,16 @@
+//
+//  FretboardValues.swift
+//  CallusLogicMobile
+//
+//  Created by Ben Gire on 4/14/18.
+//  Copyright © 2018 Gire. All rights reserved.
+//
+
+import Foundation
+
+enum FretboardValues: Int {
+    case notesPerString = 16
+    case totalNotesOnFretboard = 96
+    case twelveFretsTotalNotesOnFretboard = 78
+    case twelveFretsNotesPerString = 13
+}
